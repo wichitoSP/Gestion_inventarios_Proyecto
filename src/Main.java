d4198b30b2543392ac975ad5051e1d0b54c86a9f
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-20 23:04:53 -0600

    Se creo la funcion de actualizar al proyecto

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,27 @@ public class Main {
                                     ", Precio: $" + precioProductos.get(i) +
                                     ", Precio con IVA: Q" + (precioProductos.get(i) * (1 + Iva)));
                         }
+
                     }
+                    break;
+
+                case 3:
+                    System.out.print("Ingrese el nombre del producto a actualizar: ");
+                    String nombreActualizar = scanner.nextLine();
+                    int indexActualizar = nombreProductos.indexOf(nombreActualizar);
+                    if (indexActualizar == -1){
+                        System.out.println("Producto no encontrado.");
+
+                    }else {
+                        System.out.print("Ingrese la nueva cantidad: ");
+                        int nuevaCantidad = scanner.nextInt();
+                        scanner.nextLine();
+
+                        cantidadesProductos.set(indexActualizar,nuevaCantidad);
+                        System.out.println("Catiddad de productos actualizados");
+                    }
+                    break;
+
 
             }
 
