ebc89ba881c1a6bed62d49afa9a2838bf89cdf5e
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-20 21:39:09 -0600

    Se realiza Proyecto para la Gestion de inventarios para tienda

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+
+            System.out.println("/n***************Menú Inventario******************");
+            System.out.println("1.Agregar Producto");
+            System.out.println("2.Mostar Producto");
+            System.out.println("3.Actualizar cantidad de un producto");
+            System.out.println("4.Elimnar Producto");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción del menu: ");
+            scanner.nextLine(); // Para el siguiente salto de linea
+
+
+
+        }
+
+    }
+}
